58ecfd5d7c69d95b766ffa40d5312b5eba7719f9
Jose <josejesuscedeno@gmail.com>
2022-01-31 20:30:20 +0000

    dejar en limpio el js del ejercico 14, aclarar los procedimientos a realizar, mirar como se codifica cada uno y condificarlos

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,2 +1,12 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+let mySelect = document.querySelector("#mySelect")
+for (let country of countries){
+    let option = document.createElement("option");
+    option.innerHTML = country;
+    mySelect.appendChild(option);
+
+}
+mySelect.addEventListener('change',(e => {
+    alert(e.target.value);
+}))
--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,13 @@
 // Your code here
+let element = document.querySelector("");
+element.parentNode.removeChild(element);
+//para borrar la class es .fa-trash
+
+//aqui abajo es para agregar un elemento
+
+let beginning = "<ul>";
+let liststring = "<li>"+"<span>"+"<i class='fa fa-trash'>"+"</i>"+"</span>"+"</li";
+let ending = "</ul>";
+
+// do not modify after this line
+document.body.innerHTML = liststring;
