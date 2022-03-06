b3b17260c6477030025524e3490a5b14b3c934a4
Jose <josejesuscedeno@gmail.com>
2022-03-06 18:46:17 +0000

    nose

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,13 +1,26 @@
 // Your code here
+//añadiendo elementos
+
+let elem =document.createElement(h1)
+elem.innerHTML = "Hello World";
+document.body.appendChild(elem)
+
+//prueba con boton
+let button = document.querySelector("#addToDo");
+button.addEventListener("click", function() {
+	let elem = document.createElement("div");
+	elem.innerHTML = "<li>"+"hola"+"</li>";
+	document.body.appendChild(elem)
+})
+//removiendo elementos
+
 let element = document.querySelector("");
 element.parentNode.removeChild(element);
 //para borrar la class es .fa-trash
 
-//aqui abajo es para agregar un elemento
-
-let beginning = "<ul>";
-let liststring = "<li>"+"<span>"+"<i class='fa fa-trash'>"+"</i>"+"</span>"+"</li";
-let ending = "</ul>";
+//let beginning = "<ul>";
+//let liststring = "<li>"+"<span>"+"<i class='fa fa-trash'>"+"</i>"+"</span>"+"</li";
+//let ending = "</ul>";
 
 // do not modify after this line
 document.body.innerHTML = liststring;
